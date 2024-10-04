c248091acee9e8d764f7780742221e33720fde3f
Eduardo Almeida <eduardo.hevangelista@gmail.com>
2024-10-04 06:32:42 +0000

    arvore(comentada)

--- Estrutura de Dados/Arvore/main.c
+++ Estrutura de Dados/Arvore/main.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct arvore{
+typedef struct arvore{  //Estrutura da Arvore
     int info;
     struct arvore* esq;
     struct arvore* dir;
 }arvore;
 
-arvore* LerArvore(FILE* arq){
+arvore* LerArvore(FILE* arq){  //Função De Leitura da Arvore(mesma função explicada na aula)
     char c;
     int n;
 
@@ -28,126 +28,126 @@ arvore* LerArvore(FILE* arq){
     }
 }
 
-void ImpPreArvore(arvore* a){
+void ImpPreArvore(arvore* a){  //Impreção Pre-ordem da Arvore
     if(a != NULL){
-        printf("%d ", a->info);
+        printf("%d ", a->info);  //Print antes de chamar a recursividade
         ImpPreArvore(a->esq);
         ImpPreArvore(a->dir);
     }
 }
 
-void ImpEmArvore(arvore* a){
+void ImpEmArvore(arvore* a){  //Impreção Em-ordem da Arvore
     if(a != NULL){
         ImpEmArvore(a->esq);
-        printf("%d ", a->info);
+        printf("%d ", a->info);  //Print entre as chamadas da recursividade
         ImpEmArvore(a->dir);
     }
 }
 
-void ImpPosArvore(arvore* a){
+void ImpPosArvore(arvore* a){  //Impreção Pos-ordem da Arvore
     if(a != NULL){
         ImpPosArvore(a->esq);
         ImpPosArvore(a->dir);
-        printf("%d ", a->info);
+        printf("%d ", a->info);  //Print após a chamada da recursividade
     }
 }
 
-bool ExisteX (arvore* a, int x){
-    if(a==NULL)
+bool ExisteX (arvore* a, int x){  //Função bool(biblioteca stdboo.h) para verificar se existe "x" na arvore
+    if(a==NULL)   
         return 0;
     else if(a->info == x)
         return 1;
     else{
-        return ExisteX(a->esq,x) || ExisteX(a->dir,x) ;
+        return ExisteX(a->esq,x) || ExisteX(a->dir,x) ;  //Retorna verdadeiro se encontrar um no valido pela direita, ou pela esquerda, ou se haver mais de um no verdadeiro
     }
 }
 
-int ContElementos(arvore* a){
+int ContElementos(arvore* a){  //Função para contar quantos elementos tem na arvore
     if(a == NULL)
         return 0;
     else{
-        return ContElementos(a->esq) + ContElementos(a->dir) + 1;
+        return ContElementos(a->esq) + ContElementos(a->dir) + 1;  //A função passa por toda arvore e em cada no ela soma +1
     }
 }
 
-void ImpFolhas(arvore* a){
+void ImpFolhas(arvore* a){    //Função que imprime as folhas da arvore
     if(a != NULL){
-        if(a->esq == NULL && a->dir == NULL){
-            printf("%d ", a->info);
+        if(a->esq == NULL && a->dir == NULL){  //Faz uma comparação para ver se a "esq" e a "dir" da arvore são nulas, a comparação vem da definição de folha
+            printf("%d ", a->info);   //Imprime o no que foi verificado corretamente a cima
         }
-        ImpFolhas(a->esq);
+        ImpFolhas(a->esq);  //Percorre toda a arvore verificando qual no é uma folha
         ImpFolhas(a->dir);
     }
 }
 int main()
 {
-    arvore* a = (arvore*)malloc(sizeof(arvore));
+    arvore* a = (arvore*)malloc(sizeof(arvore)); //Cria uma estrutura(arvore) com o nome "a"
     int menu = 0;
-    while (menu != 6)
-    {
+    while (menu != 6) //Inicio do loop para manter o programa aberto e ser possivel fazer multiplas operação sem fecha-lo
+    {                 //Uso a propria variavel para a escolha de operação, o que vai me permitir controlar mais facil o fechamento da lista ao decorrer do programa
         printf("\x1B[2J\x1b[H");  //Comando que eu aprendi com um professor que tive aula na UFJF para limpar a tela
         printf("Oque Deseja Fazer?\n\n");
-        printf("1- Ler uma Arvore\n2- Imprimir Arvore\n3- Verificar Elemento\n4- Tamanho Da Arvore\n5- Imprimir Folhas\n6- Sair\n\nDigite Aqui: ");
-        scanf("%d", &menu);
+        printf("1- Ler uma Arvore\n2- Imprimir Arvore\n3- Verificar Elemento\n4- Tamanho Da Arvore\n5- Imprimir Folhas\n6- Sair\n\nDigite Aqui: "); //Menu principal de escolha
+        scanf("%d", &menu); //A leitura de numero mandara para o if correspondente a operação
 
-        if(menu == 1){
-            printf("\x1B[2J\x1b[H");
-            FILE* arq = fopen("Arvore.txt", "rt");
-            a = LerArvore(arq);
-            fclose(arq);
-            printf("Arquivo Lido com sucesso!\n\n");
-            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");
+        if(menu == 1){  //Leitura da arvore
+            printf("\x1B[2J\x1b[H");  //Limpa a tela
+            FILE* arq = fopen("Arvore.txt", "rt");  // Abre o arquivo no modo leitura
+            a = LerArvore(arq);   //Chama a função para ler o arquivo e armazena na estrutura(arvore) "a"
+            fclose(arq); //Fecha o arquivo, pois ja foi usado
+            printf("Arquivo Lido com sucesso!\n\n");  //Mostra na tela que foi possivel ler o arquivo com succeso
+            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");  //Escolhas para voltar ao menu principal de escolha ou fechar o programa
             scanf("%d", &menu);
         }
-        if(menu == 2){
+        if(menu == 2){  //Impreção da arvore
             int m;
-            printf("\x1B[2J\x1b[H");
-            printf("Qual forma de impressão voce quer?\n\n");
-            printf("1- Pre-ordem \n2- Em-ordem \n3- Pos-ordem\n\nDigite Aqui: ");
+            printf("\x1B[2J\x1b[H"); //Limpa a tela
+            printf("Qual forma de impressão voce quer?\n\n"); 
+            printf("1- Pre-ordem \n2- Em-ordem \n3- Pos-ordem\n\nDigite Aqui: "); //Escolha de qual impressão voce quer
             scanf("%d", &m);
-            printf("\x1B[2J\x1b[H");
-            if(m == 1){
+            printf("\x1B[2J\x1b[H"); //Limpa a tela
+            if(m == 1){  //Imprime a arvore em Pre-ordem
                 printf("Pre-Ordem:\n");
                 ImpPreArvore(a);
-            }else if(m == 2){
+            }else if(m == 2){  //Imprime a arvore em Em-ordem
                 printf("Em-Ordem:\n");
                 ImpEmArvore(a);
-            }else if(m == 3){
+            }else if(m == 3){  //Imprime a arvore em Pos-ordem
                 printf("Pos-Ordem:\n");
                 ImpPosArvore(a);
             }
-            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");
+            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");  //Escolhas para voltar ao menu principal de escolha ou fechar o programa
             scanf("%d", &menu);
         }
-        if(menu == 3){
+        if(menu == 3){  //Verifica se x elemento esta na arvore
             int x;
-            printf("\x1B[2J\x1b[H");
-            printf("Qual elemento voce deseja que seja verificado a exitencia?\n\nDigite aqui: ");
+            printf("\x1B[2J\x1b[H"); //Limpa a tela
+            printf("Qual elemento voce deseja que seja verificado a exitencia?\n\nDigite aqui: "); //Pede para digitar o elemento que deseja ser verificado
             scanf("%d", &x);
-            printf("\x1B[2J\x1b[H");
-            if(ExisteX(a, x))
-                printf("O elemento %d eh um no da arvore!", x);
+            printf("\x1B[2J\x1b[H"); //Limpa a tela
+            if(ExisteX(a, x)) //Verifica se o elemento esta na arvore
+                printf("O elemento %d eh um no da arvore!", x); //Imprime uma mensagem dizendo que esta na arvore
             else
-                printf("O elemento %d nao esta presente na arvore!", x);
-            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");
+                printf("O elemento %d nao esta presente na arvore!", x); //Se nao, imprime uma mensagem dizendo que nao foi encontrado na arvore
+            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");  //Escolhas para voltar ao menu principal de escolha ou fechar o programa
             scanf("%d", &menu);
         }
-        if(menu == 4){
-            printf("\x1B[2J\x1b[H");
-            printf("A arvore tem %d elementos!", ContElementos(a));
-            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");
+        if(menu == 4){  //Conta quantos elementos tem na arvore
+            printf("\x1B[2J\x1b[H");  //Limpa a tela
+            printf("A arvore tem %d elementos!", ContElementos(a));  //Usa a funcao para contar quantos elementos existem na arvore e imprime na tela
+            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");  //Escolhas para voltar ao menu principal de escolha ou fechar o programa
             scanf("%d", &menu);
         }
-        if(menu == 5){
+        if(menu == 5){  //Imprime as folhas
             printf("\x1B[2J\x1b[H");
-            printf("Folhas da arvore\n");
-            ImpFolhas(a);
-            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");
+            printf("Folhas da arvore\n"); 
+            ImpFolhas(a);  //Usa a funcao vista anteriormente para imprimir todos os nos folha da arvore
+            printf("\n\n1- Voltar ao menu\n6- Sair\n\nDigite Aqui: ");  //Escolhas para voltar ao menu principal de escolha ou fechar o programa
             scanf("%d", &menu);
         }
     }
-    free(a);
-    printf("\x1B[2J\x1b[H");
-    printf("Programa Fechado!\n");
+    free(a); //Libera a memoria alocada para "a"
+    printf("\x1B[2J\x1b[H"); //Limpa a tela
+    printf("Programa Fechado!\n"); //Imprime uma mensagem dizendo que o programa fechou
     return 0;
 }
